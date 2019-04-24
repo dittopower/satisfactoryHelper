25b9ba1eb9bd64395f325d46d05c40ed70f2da02
Damon <dittopower@gmail.com>
2019-04-24 15:02:45 +1000

    Alternate method

--- factory.js
+++ factory.js
@@ -25,7 +25,7 @@ fetch(`${location.protocol}//${location.host}/recipes.json`).then(
         for (let recipe in recipes) {
             let entry = document.createElement("li");
             entry.textContent = `${recipe}`;
-            let colours = calcTierColour(craftingtiers, recipes[recipe]["craftingtier"]);
+            let colours = calcTierColour(craftingtiers, getRecipe(recipe)["craftingtier"]);
             entry.style.backgroundColor = `rgba(${colours["red"]},${colours["green"]},${colours["blue"]},0.5)`;
             sidebar.appendChild(entry);
         }
@@ -57,7 +57,7 @@ function updateRecipeDisplay(data) {
             // For ingredients in the recipe
             let li = document.createElement("li");
             li.textContent = `${item} ${recipe[item]}`;
-            rows[entry][recipes[item]["craftingtier"]].appendChild(li);
+            rows[entry][getRecipe(item)["craftingtier"]].appendChild(li);
         }
     }
 
@@ -125,31 +125,29 @@ function calcTiers() {
     allowedCalcs--;
     let changed = 0;
     for (recipe in recipes) {
-        let init = recipes[recipe]["craftingtier"];
+        let init = getRecipe(recipe)["craftingtier"];
 
-        for (let comp in recipes[recipe]) {
+        for (let comp in getRecipe(recipe)) {
             let num = 0;
             if (comp == "craftingtier") {
                 break
             }
-            for (let prop in recipes[recipe][comp]) {
+            for (let prop in getRecipeOption(recipe,comp)) {
                 if (recipes[prop]) {
-                    num = Math.max(num, 1, recipes[prop]["craftingtier"] + 1);
+                    num = Math.max(num, 1, getRecipe(prop)["craftingtier"] + 1);
                 }
             }
-            if (recipes[recipe]["craftingtier"]) {
+            if (getRecipe(recipe)["craftingtier"]) {
                 if (init != num) {
-                    //                     console.log(`Setting '${recipe}' to tier ${Math.min(num, recipes[recipe]["craftingtier"])}`);
-                    recipes[recipe]["craftingtier"] = Math.min(num, recipes[recipe]["craftingtier"]);
+                    getRecipe(recipe)["craftingtier"] = Math.min(num, getRecipe(recipe)["craftingtier"]);
                 }
             } else {
-                //                 console.log(`Setting '${recipe}' to tier ${num}`);
-                recipes[recipe]["craftingtier"] = num;
+                getRecipe(recipe)["craftingtier"] = num;
             }
             craftingtiers = Math.max(num + 1, craftingtiers);
         }
 
-        if (init != recipes[recipe]["craftingtier"]) {
+        if (init != getRecipe(recipe)["craftingtier"]) {
             changed++;
         }
     }
@@ -160,14 +158,12 @@ function calcTiers() {
 }
 
 function selectRecipe(event) {
-    //     console.log("click",event);
     // TODO: allow the disabling of certain alternate recipes
     if (event.target.tagName == "LI") {
         clearDisplay();
         // TODO: use worker for calculations
         window.requestIdleCallback(() => {
             let current = event.target.textContent;
-            //         table.textContent=JSON.stringify(recipes[current]);
             let result = addRecipeStage(current, outputQuantity.value);
             console.log(current, JSON.stringify(result));
             window.requestIdleCallback(() => { updateRecipeDisplay(result) });
@@ -176,10 +172,9 @@ function selectRecipe(event) {
 }
 
 function addRecipeStage(data, quantity) {
-    // TODO: build a recipe chain rather than a parts list
     let versions = [];
 
-    let recipeOptions = recipes[data];
+    let recipeOptions = getRecipe(data);
     for (let opt in recipeOptions) {
         if (!recipeOptions[opt] || recipeOptions[opt].constructor.name != "Object") {
             break;
@@ -196,7 +191,7 @@ function addRecipeStage(data, quantity) {
         //         Get the component stages
         for (let component in recipe) {
             let localvariations = [];
-            if (recipes[component]) {
+            if (getRecipe(component)) {
                 let subcomponents = addRecipeStage(component, recipe[component] * multiple);
                 for (let thing in subcomponents) {
                     for (let item in variations) {
@@ -232,5 +227,111 @@ function jsonAdd(to, from) {
     return to;
 }
 
-// TODO: DM functions to seperate datastore from logic
-// i.e. getRecipe(string), 
+function getRecipe(data){
+    return recipes[data];
+}
+function getRecipeOption(data,option){
+    return getRecipe(data)[option];
+}
+function isRecipe(data){
+    return !!getRecipe(data);
+}
+function isRecipeOption(data,option){
+    return !!getRecipeOption(data,option);
+}
+function getRecipeCraftingTier(data){
+    return localStorage.getItem(`${data}.CraftingTier`);
+}
+function setRecipeCraftingTier(data,tier){
+    return localStorage.setItem(`${data}.CraftingTier`,tier);
+}
+
+
+function exploreRecipe(data, quantity) {
+    console.warn("exploreRecipe",data)
+    let versions = [];
+
+    let recipeOptions = getRecipe(data);
+    for (let opt in recipeOptions) {
+        if (!recipeOptions[opt] || recipeOptions[opt].constructor.name != "Object") {
+            break;
+        }
+        versions.push(exploreRecipeOption(data,quantity,opt))
+    }
+    console.warn(`exploreRecipe: '${data}'`, JSON.stringify(versions));
+    return versions;
+}
+
+function exploreRecipeOption(data, quantity, option) {
+    console.warn(`exploreRecipeOption: '${data}' Opt: '${option}'`);
+    if (!isRecipe(data) || !isRecipeOption(data,option) || getRecipeOption(data,option).constructor.name != "Object") {
+        throw new Error("Invalid Recipe Option");
+    }
+
+    let versions = [];
+    let recipe = getRecipeOption(data,option);
+    // Determine the number of times this recipe needs to be executed.
+    let multiple = Math.ceil(quantity / recipe["Makes"]);
+    versions.push({
+            name: data,
+            quantity: recipe["Makes"]*multiple,
+            recipe: option
+        });
+    
+
+    // Is this a harvest/gather or a craft?
+    if(recipe["Harvest"]){
+        console.log("harvest");
+    }else{
+        console.log("craft");
+        versions.push(new Array());
+
+        //         Get the ingredient stages
+        for (let ingredient in recipe) {
+            if (getRecipe(ingredient)) {
+                let tempClone = Array.from(versions);
+                versions[1] = new Array();
+
+                let ingredientRecipes = exploreRecipe(ingredient, recipe[ingredient] * multiple);
+                ingredientRecipes.forEach((res)=>{console.log("ingredientRecipes",JSON.stringify(res));});
+
+                console.log("tempClone[1]",JSON.stringify(tempClone[1]));
+                for (let ingredientOption = 0; ingredientOption < ingredientRecipes.length; ingredientOption++) {
+                    console.log("ingredientOption",ingredientOption);
+                    console.log("versions",JSON.stringify(versions));
+
+                    // Additional Ingredients
+                    for (let i = 0; i < ingredientRecipes[ingredientOption].length; i++){
+                        if (ingredientRecipes[ingredientOption][i]){
+                            let ref = i+1;
+                            console.log("..joining", JSON.stringify(ingredientRecipes[ingredientOption][i]))
+
+                            if(i == 0){
+                                if((!tempClone[ref]) || tempClone[ref].length == 0){
+                                    versions[ref].push( [ingredientRecipes[ingredientOption][i]]);
+                                }else{
+                                    for( let existing in  tempClone[ref]){
+                                        versions[ref].push( (tempClone[ref][existing] ? tempClone[ref][existing] : []).concat([ingredientRecipes[ingredientOption][i]]));
+                                    }
+                                }
+
+                            }else{
+                                if((!versions[ref]) || versions[ref].length == 0){
+                                    versions[ref] = [ingredientRecipes[ingredientOption][i]];
+                                }else{
+                                    versions[ref] = (versions[ref] ? versions[ref] : []).concat([ingredientRecipes[ingredientOption][i]]);
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
+    
+    console.warn(`exploreRecipeOption return: '${data}' Opt: '${option}'`, JSON.stringify(versions));
+    return versions;
+}
+
