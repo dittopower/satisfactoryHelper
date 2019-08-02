d54db33186cd366479751e0f02a0fdb6b6a76f5b
Damon <dittopower@gmail.com>
2019-08-02 22:08:21 +1000

    init styles

--- satisfactoryHelper/src/components/header/index.js
+++ satisfactoryHelper/src/components/header/index.js
@@ -4,11 +4,11 @@ import style from './style';
 
 const Header = () => (
 	<header class={style.header}>
-		<h1>Preact App</h1>
+		<h1>Satisfactory Helper</h1>
 		<nav>
-			<Link activeClassName={style.active} href="/">Home</Link>
-			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/john">John</Link>
+			<Link activeClassName={style.active} href="/">Recipe Explorer</Link>
+			<Link activeClassName={style.active} href="/profile">Visual Explorer</Link>
+			<Link activeClassName={style.active} href="/profile/john">Recipe Manager</Link>
 		</nav>
 	</header>
 );
